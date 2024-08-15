7bf8a867d5869a41c132b697293ab9a9fa223563
Richard Alison <richardalison86@gmail.com>
2024-08-15 17:59:19 +0900

    feat(misc/macros): add n-dimensional vector macro

--- src/misc/macros.rs
+++ src/misc/macros.rs
@@ -1,3 +1,15 @@
+#[macro_export]
+macro_rules! ndvec {
+    ($init:expr; $dim:expr) => {
+        vec![$init; $dim]
+    };
+    ($init:expr; $cur_dim:expr, $($rest_dim:expr),*) => {
+        vec![ndvec![$init; $($rest_dim),*]]
+    };
+}
+
+pub use ndvec;
+
 // What the fuck???
 // https://github.com/rust-lang/rust/issues/35853#issuecomment-415993963
 #[macro_export]
